aab6bb39c2988e788ffedc2fd5dc7a2b9fef4c3e
Yixing-Chen-Shawn <shawnchenac@gmail.com>
2020-09-02 23:57:34 -0400

    Wed Sep 2 2020 Finished all login function and registration function for the project -- Yixing

---
+++ app/src/main/java/com/CIS400/fever_detection_app/ForgetPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.CIS400.fever_detection_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
---
+++ app/src/main/java/com/CIS400/fever_detection_app/data/MyUser.java
@@ -0,0 +1,4 @@
+package com.CIS400.fever_detection_app.data;
+
+public class MyUser {
+}
