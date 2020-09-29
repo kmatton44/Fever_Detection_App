21593817cdcdeddec614af349c68f164944556d6
Yixing-Chen-Shawn <shawnchenac@gmail.com>
2020-09-29 00:24:29 -0400

    Sep 28 Sun finished UI for me page and heart rate --YIXING

---
+++ app/src/main/java/com/CIS400/fever_detection_app/activity/HeartRateLogActivity.java
@@ -0,0 +1,17 @@
+package com.CIS400.fever_detection_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.CIS400.fever_detection_app.R;
+
+public class HeartRateLogActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heartratelog);
+        
+    }
+}
---
+++ app/src/main/java/com/CIS400/fever_detection_app/activity/MeChangePasswordActivity.java
@@ -0,0 +1,16 @@
+package com.CIS400.fever_detection_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.CIS400.fever_detection_app.R;
+
+public class MeChangePasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_change_password);
+    }
+}
